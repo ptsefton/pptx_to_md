92dbd3309e02ea5f5fc2f7dde328345bd153b292
Peter Sefton <peter.sefton@uts.edu.au>
2019-11-07 08:58:42 +1100

    Initial upload - trying out pipenv

--- README.md
+++ README.md
@@ -1,2 +1,74 @@
 # pptx_to_md
+
 Powerpoint to Markdown converter - Python 3 script
+
+
+
+## Audience
+
+This is for people who know how to run Python, instructions are for MacOs.
+
+## About 
+
+This is a simple rough and ready way to turn a Powerpoint slide deck into Markdown. It will convert the slides to PNG format (one per slide), put any text it can find on the slide into the alt attribute, and extract slide notes as a set of plain-text paragraphs.
+
+Hint: Format your slide notes as Markdown/Commonmark for instant gratification
+
+## Usage
+
+NOTE: This works on `.pptx` files ONLY - if you're using Google Drive or Keynote etc export your presentation to .pptx first.
+
+The first step is to save the slides as PNG images. You can try to do this automatically (see below) but it's actually just as fast to do it manually.
+
+### Manually generate images
+
+* Open the presentation.
+
+* From the `File` menu select `Export...` and choose `PNG` in `File Format` dropdown - also select `Save Every Slide`.
+
+*  Click `Replace`
+
+* Click OK on the confirmation dialogue "Each slide in your presentation has been saved as a separate file in the folder /your/path/presentation".
+
+You should now have a directory named for your pptx file minus the extension.
+
+### Or,automatically generate images
+
+If you're on a Mac, and you have Powerpoint, try running with the -p flag:
+
+   python3 pres2md.py -p your-preso.pptx 
+
+The first time you access a file, the operating system will make you do an authentication step.
+
+*  If the script fails try running it again. 
+
+*  If it stalls for more than a few seconds try clicking the dialogue boxes yourself.
+
+
+### Generate the markdown file
+
+The second and final step is to generate the Markdown/Commonmark file.   
+
+When you have a directory with the same name as your .pptx file (minus the extension) run the code with your.
+
+    python3 pres2md.py  your-preso.pptx 
+
+
+If you're using Pelican you can also add a prefix to the image paths:
+
+    python3 pres2md.py -i {static} your-preso.pptx 
+
+
+
+
+
+
+
+
+## Install it
+
+Make a virtual environment
+
+Install the dependencies
+
+
---
+++ pres2md.py
@@ -0,0 +1,109 @@
+ #!/usr/bin/env/python
+import argparse
+import subprocess
+import os
+import html
+import parser
+import os.path
+from pptx import Presentation
+import html
+import math
+
+def get_text(slide):
+    text = ""
+    for shape in slide.shapes:
+        if not shape.has_text_frame:
+            continue
+        for paragraph in shape.text_frame.paragraphs:
+            for run in paragraph.runs:
+                text += run.text
+            text += "\n"
+    return(text)
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('filename',  default=None, help='Name of input Powerpoint file')
+parser.add_argument('-i', '--img-prefix', default = '', help="String to put in front of image paths")
+parser.add_argument('-p,', '--powerpoint', default = False, action="store_true", help="Attempt to presentation to png images using Powerpoint first")
+
+args = vars(parser.parse_args())
+import glob
+import shutil
+
+
+img_prefix = args["img_prefix"]
+filename = os.path.abspath(args["filename"])
+path, name = os.path.split(filename)
+stem, _ = os.path.splitext(name)
+out_dir = os.path.join(path, stem)
+
+md_path = os.path.join(out_dir, "index.md")
+
+
+def save_images_using_powerpoint():
+
+    ppt_command = 'osascript pptx_to_png_powerpoint.scrpt "%s"  "%s" "%s"' % (out_dir, filename, stem)
+    
+    subprocess.Popen(ppt_command,
+                               shell=True,
+                               stdout=subprocess.PIPE).communicate()
+
+
+
+
+def parse_preso():
+    prs = Presentation(filename)
+
+    slides = []
+    for slide in prs.slides:
+        s = {"text": "", "notes": ""}
+        s["text"] = get_text(slide)
+        if slide.has_notes_slide:
+            s["notes"] = get_text(slide.notes_slide)
+        slides.append(s)
+
+    md = """---
+    title:  >
+      Untitled
+    date:
+    slug: %s
+    category:
+    author:
+---
+
+
+
+    """ % (stem)
+
+
+    count = 0
+    digits = math.floor(math.log10(len(slides))) + 1
+    formatString = "Slide%0" + str(digits) + "d.png"
+    for slide in slides:
+        count += 1
+        # TODO make this a functions
+        
+        image_path = formatString % count
+        md += """
+    <section typeof='http://purl.org/ontology/bibo/Slide'>
+    <img src='%s%s' alt='%s%s' title='%s' border='1'  width='85%%'/>
+
+
+    %s
+
+
+
+    </section>
+
+            """  % (img_prefix, image_path, html.escape(slide["text"], quote=True), html.escape(slide["text"], quote=True), str(count), slide["notes"])
+    return md
+
+
+if args["powerpoint"]:
+    save_images_using_powerpoint()
+else:
+    md = parse_preso()
+
+with open(md_path, 'w') as o:
+    print("writing", md_path)
+    o.write(md)
