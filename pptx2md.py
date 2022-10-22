dd1a6a12abc687df66d467abe904984b48a0ebc9
Mike Lynch <mike@mikelynch.org>
2022-10-22 14:28:58 +1100

    Switched file handling to pathlib and refactored things a bit, made it work with a pdf at the same level as the pptx, copy the pdf and pptx versions at the same time, fixed bug where the pptx was being copied as ".pptx"

--- pptx2md.py
+++ pptx2md.py
@@ -3,7 +3,7 @@ import argparse
 import subprocess
 import os
 import html
-import os.path
+from pathlib import Path
 from pptx import Presentation
 import html
 import math
@@ -21,46 +21,23 @@ def get_text(slide):
     return(text)
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument('filename',  default=None, help='Name of input Powerpoint file')
-parser.add_argument('-i', '--img-prefix', default = '', help="String to put in front of image paths")
-parser.add_argument('-p,', '--pdf', default = False, action="store_true", help="Convert PDF version of presentation to png images using Imagemagick convert command")
-parser.add_argument('-t,', '--topdf', default = False, action="store_true", help="Convert DOCX version to PDF using openoffice (soffice)")
-parser.add_argument('-s,', '--soffice', default = "soffice", help="soffice command")
 
-args = vars(parser.parse_args())
-
-
-
-img_prefix = args["img_prefix"]
-filename = os.path.abspath(args["filename"])
-path, name = os.path.split(filename)
-stem, pptx_filename = os.path.splitext(name)
-out_dir = os.path.join(path, stem)
-md_path = os.path.join(out_dir, "index.md")
-pdf_filename = stem + ".pdf"
-
-
-
-def save_images_from_pdf(formatString):
+def save_images_from_pdf(out_dir, pdf_filename, formatString):
     os.chdir(out_dir)
     convert_command = f"""convert  "{pdf_filename}" {formatString}"""
     print(convert_command)
     subprocess.Popen(convert_command,
                     shell=True,
                     stdout=subprocess.PIPE).communicate()
-    
 
 
 
-def parse_preso():
-    try:
-        os.mkdir(out_dir)
-    except:
-        pass
-    shutil.copyfile(filename, os.path.join(out_dir, pptx_filename))
 
-    prs = Presentation(filename)
+def parse_preso(powerpoint_file):
+    slug = powerpoint_file.stem
+    pptx = powerpoint_file.name
+    pdf = powerpoint_file.with_suffix('.pdf').name
+    prs = Presentation(powerpoint_file)
     title = prs.core_properties.title
     slides = []
     for slide in prs.slides:
@@ -74,17 +51,15 @@ def parse_preso():
     title:  >
       {title}
     date:
-    slug: {stem}
+    slug: {slug}
     category:
     author:
 ---
 
-<a href="{img_prefix}{pdf_filename}">PDF version</a> | <a href="{img_prefix}{pptx_filename}">Powerpoint Version</a>
+<a href="{img_prefix}{pdf}">PDF version</a> | <a href="{img_prefix}{pptx}">Powerpoint Version</a>
 
 
     """
-
-
     
     digits = math.floor(math.log10(len(slides))) + 1
     count = -1
@@ -110,26 +85,49 @@ def parse_preso():
 """ 
     return md, formatString
 
-md, formatString = parse_preso()
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filename',  default=None, type=Path, help='Name of input Powerpoint file')
+    parser.add_argument('-i', '--img-prefix', default = '', help="String to put in front of image paths")
+    parser.add_argument('-p,', '--pdf', default = False, action="store_true", help="Convert PDF version of presentation to png images using Imagemagick convert command")
+    parser.add_argument('-t,', '--topdf', default = False, action="store_true", help="Convert DOCX version to PDF using openoffice (soffice)")
+    parser.add_argument('-s,', '--soffice', default = "soffice", help="soffice command")
+
+    args = vars(parser.parse_args())
+
+    img_prefix = args["img_prefix"]
+    powerpoint_file = args["filename"].resolve()
+    base_path = powerpoint_file.parent;
+    pdf_file = powerpoint_file.with_suffix(".pdf")
+    out_dir = base_path / powerpoint_file.stem
+    md_path = out_dir / "index.md"
+
+    if not powerpoint_file.is_file():
+        raise Exception(f"{powerpoint_file} file not found")
+
+    out_dir.mkdir(parents=True, exist_ok=True)
+
+    shutil.copyfile(powerpoint_file, out_dir / powerpoint_file.name)
+    if args["pdf"]:
+        shutil.copyfile(pdf_file, out_dir / pdf_file.name)
+
+    md, formatString = parse_preso(powerpoint_file)
     
+    with open(md_path, 'w', encoding='utf-8') as o:
+        print("writing", md_path)
+        o.write(md)
 
-with open(md_path, 'w') as o:
-    print("writing", md_path)
-    o.write(str(md.encode('utf-8')))
-
-if args["topdf"]:
-    os.chdir(out_dir)
-    convert_command =  f'{args["soffice"]} --headless --convert-to pdf "{filename}"'
-    print(convert_command);
-    subprocess.Popen(convert_command,
-                    shell=True,
-                    stdout=subprocess.PIPE).communicate()
-    save_images_from_pdf(formatString)
-
-
-elif args["pdf"]:
-    save_images_from_pdf(formatString)
+    if args["topdf"]:
+        os.chdir(out_dir)
+        convert_command =  f'{args["soffice"]} --headless --convert-to pdf "{filename}"'
+        print(convert_command);
+        subprocess.Popen(convert_command,
+                        shell=True,
+                        stdout=subprocess.PIPE).communicate()
+        save_images_from_pdf(out_dir, pdf_file, formatString)
 
 
-
+    elif args["pdf"]:
+        save_images_from_pdf(out_dir, pdf_file, formatString)
 
